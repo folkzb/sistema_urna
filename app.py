2299e0cfd7cccea17dff62847e4e1cdadadc388d
brunocosta <brunocosta@luizalabs.com>
2023-01-17 18:35:00 -0300

    alterações pertinentes ao programa

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask, redirect, url_for, request, render_template
 import requests
 
+
+imagem = f'http://127.0.0.1:8000/'
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return redirect(url_for('static',filename='index.html'))
+    print(imagem)
+    return render_template('index.html', imagem=imagem) 
+
+
 
 @app.route('/numero')
 def solicitado():
+    global imagem
     argumentos = request.args.to_dict()
     numero = argumentos['numero']
     imagem = requests.get(f'http://127.0.0.1:8080/read_numero/{numero}')
     nome = imagem.json()
     nome = nome[0][3]
-    imagem = requests.get(f'http://127.0.0.1:8000/{nome}')
-    #return redirect('/')
-    return redirect(url_for('static',filename='index.html'))
+    imagem = f'http://127.0.0.1:8000/{nome}'
+    return redirect('/')
+    #return render_template('index.html', imagem=imagem) 
 
 
 if __name__ == '__main__':
